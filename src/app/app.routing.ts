78b8f53e67cb4ca78cfaf26f0b6ab6609793ce31
SATHISH M <sathish.mp@SATHISH-MANI.local>
2018-07-30 14:27:22 +0530

    all-done

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component'
 import { HomePageComponent } from './home-page/home-page.component'
+import { ProductPageComponent } from './product-page/product-page.component';
 
 
 
@@ -9,6 +10,7 @@ const appRoutes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: HomePageComponent},
   { path:'login', component:LoginPageComponent},
+  { path:'product', component:ProductPageComponent},
   { path: '**', redirectTo: 'dashboard',   }
 ];
 
